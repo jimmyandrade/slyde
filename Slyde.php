d31a09351bf447aeb98f54f89fc9a24a42a1d089
Paulo (Jimmy) Andrade <paulojimmyandrade@icloud.com>
2014-05-24 23:11:40 -0300

    Loader path change

--- Slyde.php
+++ Slyde.php
@@ -3,11 +3,11 @@
  * Plugin Name: Slyde
  * Plugin URI: http://github.com/jimmyandrade/slyde
  * Description: A flexible WordPress plugin for slides management
- * Version: 1.0.0
+ * Version: 1.0.2
  * Author: Paulo H. Jimmy Andrade Mota C.
  * Author URI: http://github.com/jimmyandrade/
  */
 use jimmyandrade\Slyde;
 use jimmyandrade\Slider_Widget;
-$loader = require 'src\jimmyandrade\Slyde.php';
+$loader = require ( plugin_dir_path( __FILE__ ) . 'src/jimmyandrade/Slyde.php' );
 Slyde::get_instance();
